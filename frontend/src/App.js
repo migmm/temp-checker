c76fed202f60139723ba5b5fe58c7ef420959668
migmm <micmigm@gmail.com>
2023-02-02 20:19:09 -0300

    Added React Boilerplate

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+
+            <div className="App-wrapper">
+                <div className="App-content">
+                    <div className="left-column"></div>
+                    <div className="middle-column"></div>
+                    <div className="right-column"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
